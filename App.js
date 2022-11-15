201ae45a8bd2f9c4ca70d1c35ae6d90b5db2790a
Jack Stubblefield <Jack011601@gmail.com>
2022-11-15 10:55:25 -0600

    data fetching works, basic list showing

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, List } from 'react-native-paper';
 import AppBar from './components/AppBar';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import { FlatList } from 'react-native-web';
 
-const [data, setData] = useState([]);
-useEffect(() => {
-  
-}, []);
+
 export default function App() {
+  const [pokeData, setPokeData] = useState([]);
+  useEffect(() => {
+    (async () => {
+
+      let response = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+      let results = response.data.results;
+      console.log(results);
+      setPokeData(results)
+    })();
+  }, []);
+
+  const renderItem = ({ item }) => (
+
+    <Button>{item.name}</Button>
+  )
+
+
   return (
     <>
       <AppBar />
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello World</Text>
+        <FlatList
+          data={pokeData}
+          renderItem={renderItem}
+        />
         <StatusBar style="auto" />
       </View>
     </>
